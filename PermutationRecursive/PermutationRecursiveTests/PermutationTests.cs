68f3e8019b1d486f5459d158216893a966d2cf86
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-02-04 10:16:19 +0800

    2nd green light

--- PermutationRecursive/PermutationRecursiveTests/PermutationTests.cs
+++ PermutationRecursive/PermutationRecursiveTests/PermutationTests.cs
@@ -24,5 +24,27 @@ namespace PermutationRecursive.Tests
             // assert
             expected.ToExpectedObject().ShouldMatch(actual);
         }
+
+        [TestMethod()]
+        public void SetSourceTest_length_3()
+        {
+            // arrange
+            List<string> source = new List<string> { "b", "c", "d" };
+            List<List<string>> expected = new List<List<string>> {
+                new List<string> { "b", "c", "d" },
+                new List<string> { "b", "d", "c" },
+                new List<string> { "c", "b", "d" },
+                new List<string> { "c", "d", "b" },
+                new List<string> { "d", "b", "c" },
+                new List<string> { "d", "c", "b" },
+            };
+
+            // act
+            Permutation target = new Permutation(source);
+            var actual = target.Result;
+
+            // assert
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
     }
 }
