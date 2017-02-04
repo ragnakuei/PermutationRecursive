0808f33ef3e17dbcba4b440f3502ec2dd39550e8
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-02-04 10:22:02 +0800

    add console output

--- PermutationRecursive/PermutationRecursive/Program.cs
+++ PermutationRecursive/PermutationRecursive/Program.cs
@@ -9,6 +9,18 @@ namespace PermutationRecursive
     {
         static void Main(string[] args)
         {
+            var source = new List<string> { "1", "2", "3", "4" };
+            Permutation permutation = new Permutation(source);
+            var results = permutation.Result;
+            foreach (var result in results)
+            {
+                foreach (var item in result)
+                {
+                    Console.Write(item+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
